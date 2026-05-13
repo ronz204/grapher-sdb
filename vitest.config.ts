20562fafdb96e7f0fea40be108ea5805632134b7
roney ruiz <roneyruizrojas@gmail.com>
2026-05-12 21:06:31 -0600

    feat: setup tspaths and vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-
+  resolve: {
+    alias: {
+      "@gql": "./source/gql",
+      "@types": "./source/types",
+    },
+  },
+  test: {
+    include: ["testing/**/*.test.ts"],
+  },
 });
