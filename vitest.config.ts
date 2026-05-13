29043cc783f0c3446059f5a56d6756467dbfe2d4
roney ruiz <roneyruizrojas@gmail.com>
2026-05-12 21:42:23 -0600

    chore: add tspath for core components

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   resolve: {
     alias: {
+      "@core": "./source/core",
+
       "@gql": "./source/gql",
       "@types": "./source/types",
     },
