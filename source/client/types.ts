1b85afc4d7b2ac50ca4cda8900c034a05fc8c632
roney ruiz <roneyruizrojas@gmail.com>
2026-05-15 20:55:41 -0600

    chore: discard complex client temporary

--- source/client/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Middleware, Transport } from "@pipelines/types";
-
-export type ClientConfig = {
-  transport: Transport;
-  headers?: Record<string, string>;
-  middleware?: Middleware[];
-};
