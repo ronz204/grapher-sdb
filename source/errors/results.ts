453dd41dc95686312e06e2773c59ee666c98cfb0
roney ruiz <roneyruizrojas@gmail.com>
2026-05-16 11:49:31 -0600

    chore: purge structure temporary

--- source/errors/results.ts
+++
@@ -1,9 +0,0 @@
-import type { GqlError } from "./types";
-
-export type Result<TData, TError = GqlError> =
-  | { ok: true; data: TData }
-  | { ok: false; error: TError };
-
-export function ok<TData>(data: unknown): Result<TData> {
-  return { ok: true, data: data as TData };
-};
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@core": "./source/core",
-      "@errors": "./source/errors",
-      "@transports": "./source/transports",
-    },
-  },
   test: {
     include: ["testing/**/*.test.ts"],
   },
